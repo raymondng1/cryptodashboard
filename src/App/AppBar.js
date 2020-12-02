763a7ba70ceee2c7887a9c774ddddfa1ba0e7fc7
Raymond Ng <raymond.ng47@gmail.com>
2020-12-01 22:22:40 -0500

    react-context

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
+import {AppContext} from "./AppProvider"
 
 const Logo = styled.div`
 font-size: 1.5 em;
@@ -24,9 +25,16 @@ function toProperCase(lower){
 
 function ControlButton({name, active}){
     return (
-        <ControlButtonElem active={active}>
+        <AppContext.Consumer>
+            {({page}, setPage ) => (
+            <ControlButtonElem 
+            active={page === name}
+            onClick={() => setPage(name)}
+            >
             {toProperCase(name)}
-        </ControlButtonElem>
+            </ControlButtonElem>
+            )}
+        </AppContext.Consumer>
     )
 }
 export default function(){
--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,17 @@
 import './App.css';
-import WelcomeMessage from './WelcomeMessage'
-import AppLayout from './AppLayout'
-import AppBar from './AppBar'
+import WelcomeMessage from './WelcomeMessage';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
+
 
 function App() {
   return (
     <AppLayout>
-      <AppBar/>
-      <div></div>
-      <WelcomeMessage/>
+      <AppProvider> 
+        <AppBar/>
+        <WelcomeMessage/>
+      </AppProvider>
     </AppLayout>
 
   );
