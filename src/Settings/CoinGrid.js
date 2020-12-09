7edb3aa72b459277b92146be95e238a4e5bad82d
Raymond Ng <raymond.ng47@gmail.com>
2020-12-08 20:54:01 -0500

    filtered-search

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -11,14 +11,19 @@ export const CoinGridStyled = styled.div`
     margin-top: 40px;
     `
 
-function getCoinsToDisplay(coinList,topSection, favorites){
-    return topSection ? favorites : Object.keys(coinList).slice(0, topSection ? 10 : 100);
+function getLowerSectionCoins(coinList, filteredCoins){
+    return (filteredCoins && Object.keys(filteredCoins)) ||
+        Object.keys(coinList).slice(0,100)
+}
+
+function getCoinsToDisplay(coinList,topSection, favorites, filterCoins){
+    return topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
 }
 
 export default function ({topSection}){
     return <AppContext.Consumer> 
-        {({coinList, favorites}) => <CoinGridStyled>
-            {getCoinsToDisplay(coinList, topSection, favorites).map(
+        {({coinList, favorites, filteredCoins }) => <CoinGridStyled>
+            {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(
                 coinKey => <CoinTile topSection={topSection} coinKey={coinKey}/>
             )}
             </CoinGridStyled>}
--- src/Settings/Search.js
+++ src/Settings/Search.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import {backgroundColor2, fontSize2} from "../Shared/Styles";
 import {AppContext } from '../App/AppProvider';
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
 
 const SearchGrid = styled.div`
     display: grid;
@@ -16,8 +18,32 @@ const SearchInput = styled.input`
     color: #1163c9;
     place-self: center left; 
 `
+
+const handleFilter = _.debounce((inputValue, coinList, setFilterCoins) =>{
+    // get all the coin symbols
+    let coinSymbols = Object.keys(coinList);
+
+    //get all the coin names, map symbol to name
+    let coinNames = coinSymbols.map(sym => coinList[sym].CoinName)
+    let allStringsToSearch = coinSymbols.concat(coinNames);
+    let fuzzyResults = fuzzy.filter(inputValue, allStringsToSearch, {}).map(result => result.string);
+    
+    let filteredCoins = _.pickBy(coinList, (result, symKey) =>{
+        let coinName = result.CoinName;
+
+        return(_.includes(fuzzyResults, symKey)) || _.includes(fuzzyResults, coinName)
+    })
+    console.log(filteredCoins)
+    setFilterCoins(filteredCoins)
+}, 500);
+
 function filterCoins(e,setFilteredCoins, coinList){
-    let inputValue = e.target.value; 
+    let inputValue = e.target.value;
+    if(!inputValue){
+        setFilteredCoins(null);
+        return 
+    }
+    handleFilter(inputValue, coinList, setFilteredCoins) 
 }
 
 export default function(){
